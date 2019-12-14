b7553922255b4d3939de7319d0f82de14cd6fc3c
fvyshkov <fvyshkov@gmail.com>
2019-12-14 16:31:26 +0300

    тестирование снапшотов, может так правильнее?

--- project/frontend/src/__tests__/SheetToolbar.spec.js
+++ project/frontend/src/__tests__/SheetToolbar.spec.js
@@ -1,28 +1,13 @@
 import React from 'react';
-import Toolbar, { Item } from 'devextreme-react/toolbar';
-import { mount } from 'enzyme';
-import SheetToolbar from "../components/SheetToolbar.jsx";
+import SheetToolbar from '../components/SheetToolbar.jsx';
+import { render, mount } from 'enzyme';
+import { renderToJson } from 'enzyme-to-json';
 
-import { shallow } from 'enzyme';
-//import MyComponent from './MyComponent';
+describe('<SheetToolbar />', () => {
+    it('Snapshot пустой', () => {
+      const wrapper = render(<SheetToolbar />);
+      expect(renderToJson(wrapper)).toMatchSnapshot();
+    });
 
-
-
-
-describe('<App />', () => {
-
-    const wrap1 = mount(<SheetToolbar />);
-
-  it('renders', () => {
-
-    expect(wrap1.find(SheetToolbar).exists()).toBe(true);
-  });
-
-  it('should be possible to activate button with Spacebar', () => {
-  const component = mount(<Toolbar />);
-  expect(component).toMatchSnapshot();
-  component.unmount();
-});
 
 });
-
